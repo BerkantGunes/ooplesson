6d8ab6605e62bc488d07b5a5cf3415186bbd17ba
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-03 18:56:49 +0300

    Add files via upload

---
+++ ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Product Add");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product Update");
+        }
+    }
+}
